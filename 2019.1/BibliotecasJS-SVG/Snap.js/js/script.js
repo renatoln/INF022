fa9dde431909915cfffca7c4b273266bec180ed6
danielfbbaptista94 <daniel94.baptista@gmail.com>
2019-06-12 22:36:08 -0300

    Add Project

---
+++ 2019.1/BibliotecasJS-SVG/Snap.js/js/script.js
@@ -0,0 +1,34 @@
+var m = Snap('#mapa');
+Snap.load('bahia-mod.svg', onSVGLoaded);
+
+
+function onSVGLoaded(data) {
+      m.append( data );
+
+      var g = m.select('#Municipios');
+
+      $.each(g.selectAll("path").items, function() {
+            
+            this.hover(
+                  function () {
+                        this.attr({ 'fill': 'red'});
+                  },
+                  function () {
+                        this.attr({ 'fill': '#EEDDB3'});
+                  }
+            )
+        
+            this.click(function() {
+                  let slicedId = this.attr('id').slice(4,11);
+                  let url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" + slicedId;
+
+                  let jsondata = fetch(url).then(
+                        function(u){ return u.json();}
+                  ).then(
+                        function(json){
+                              alert('Id: ' + json.id + '\nCidade: ' + json.nome );
+                        }
+                  )
+            });
+        });
+}
