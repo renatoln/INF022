d18b68fa12aa01a882d913dca87166e1f3fd2cd7
danielfbbaptista94 <daniel94.baptista@gmail.com>
2019-06-18 17:34:36 -0300

    Add select field para fazer a transação entre municipios, meso e microregiões

---
+++ 2019.1/BibliotecasJS-SVG/Snap.js/js/meso-script.js
@@ -0,0 +1,25 @@
+var m = Snap('#mapa');
+Snap.load('/maps/mesorregioes.svg', onSVGLoaded);
+
+function onSVGLoaded(data) {
+    m.append( data );
+    
+    var g = m.select('#Microrregioes');
+    
+    $.each(g.selectAll("path").items, function() {
+          
+          this.click(function() {
+                let slicedId = this.attr('id').slice(4,11);
+                let url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" + slicedId;
+                
+                let jsondata = fetch(url).then(
+                      function(u){ return u.json();}
+                ).then(
+                      function(json){
+                            alert('Id: ' + json.id + '\nCidade: ' + json.nome );
+                      }
+                )
+          });
+    });
+    
+}
--- 2019.1/BibliotecasJS-SVG/Snap.js/js/script.js
+++ 2019.1/BibliotecasJS-SVG/Snap.js/js/script.js
@@ -1,12 +1,11 @@
 var m = Snap('#mapa');
-Snap.load('bahia-mod.svg', onSVGLoaded);
-
+Snap.load('/maps/bahia-mod.svg', onSVGLoaded);
 
 function onSVGLoaded(data) {
       m.append( data );
-
+      
       var g = m.select('#Municipios');
-
+      
       $.each(g.selectAll("path").items, function() {
             
             this.hover(
@@ -17,11 +16,11 @@ function onSVGLoaded(data) {
                         this.attr({ 'fill': '#EEDDB3'});
                   }
             )
-        
+            
             this.click(function() {
                   let slicedId = this.attr('id').slice(4,11);
                   let url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" + slicedId;
-
+                  
                   let jsondata = fetch(url).then(
                         function(u){ return u.json();}
                   ).then(
@@ -30,5 +29,6 @@ function onSVGLoaded(data) {
                         }
                   )
             });
-        });
+      });
+      
 }
