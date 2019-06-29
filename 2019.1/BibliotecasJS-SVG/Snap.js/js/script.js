9db0d5ec890028f1878fc818da743b66b47091e2
Alison Morais <armorais01@gmail.com>
2019-06-29 12:42:09 -0300

    Adicionado microrregioes e mesorregioes no tooltip dos municipios

--- 2019.1/BibliotecasJS-SVG/Snap.js/js/script.js
+++ 2019.1/BibliotecasJS-SVG/Snap.js/js/script.js
@@ -3,28 +3,31 @@ Snap.load('maps/bahia-mod.svg', onSVGLoaded);
 
 function onSVGLoaded(data) {
       m.append( data );
-      
+
       var g = m.select('#Municipios');
-      
+
       $.each(g.selectAll("path").items, function() {
                   this.hover(
                   function () {
                         let slicedId = this.attr('id').slice(4,11);
-                        let url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" + slicedId;                    
+                        let url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" + slicedId;
                         let jsonResponse;
 
                         let jsondata = fetch(url)
                         .then(res => res.json())
                         .then(data => jsonResponse = data)
-                        .then(() => this.append(Snap.parse('<title>'+ jsonResponse.nome +'</title>')))
-                        
+                        .then(() => this.append(Snap.parse('<title><p> Cidade: ' + jsonResponse.nome
+                                    + '&#013 Microrregião: ' + jsonResponse.microrregiao.nome
+                                    + '&#013 Mesorregião: ' + jsonResponse.microrregiao.mesorregiao.nome
+                                    + '</title>')))
+
                         this.attr({ 'fill': 'red' });
                   },
                   function () {
                         this.attr({ 'fill': '#EEDDB3'});
                   }
             )
-            
+
             this.click(function() {
                   let slicedId = this.attr('id').slice(4,11);
                   let url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" + slicedId;
