14b99a013157125e48d06490c39102e1c5e7436e
danielfbbaptista94 <daniel94.baptista@gmail.com>
2019-07-03 16:31:12 -0300

    Arrumado mousehover de meso-script.js

--- 2019.1/BibliotecasJS-SVG/Snap.js/js/meso-script.js
+++ 2019.1/BibliotecasJS-SVG/Snap.js/js/meso-script.js
@@ -4,11 +4,21 @@ Snap.load('maps/mesorregioes.svg', onSVGLoaded);
 function onSVGLoaded(data) {
     m.append( data );
     
-    var g = m.select('#Microrregioes');
+    var g = m.select('#Mesorregioes');
     
     $.each(g.selectAll("path").items, function() {
+            this.attr( { 'style' :  "fill:#EEDDB3" }  ); 
+
+            this.hover(
+                  function() {
+                        this.attr({ 'style' :  "fill:red" });
+                  },
+                  function () {
+                        this.attr({ 'style' :  "fill:#EEDDB3" });
+                  }
+            )
           
-          this.click(function() {
+            this.click(function() {
                 let slicedId = this.attr('id').slice(4,11);
                 let url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" + slicedId;
                 let jsondata = fetch(url).then(
@@ -18,7 +28,7 @@ function onSVGLoaded(data) {
                             alert('Id: ' + json.id + '\nCidade: ' + json.nome );
                       }
                 )
-          });
+            });
     });
     
 }
