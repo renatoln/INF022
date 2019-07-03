1f9584bae86176db3cc9aabf7691204496cf82e9
Alison Morais <armorais01@gmail.com>
2019-07-03 19:08:10 -0300

    Adicionado preparacao para mudancas de SVGs e otimizacao de funcoes

--- 2019.1/BibliotecasJS-SVG/Snap.js/js/script.js
+++ 2019.1/BibliotecasJS-SVG/Snap.js/js/script.js
@@ -1,42 +1,51 @@
-var m = Snap('#mapa');
+var mapa = Snap('#mapa');
+
 Snap.load('maps/bahia-mod.svg', onSVGLoaded);
 
 function onSVGLoaded(data) {
-      m.append( data );
+      var url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/";
+      var jsonResponse;
+      var bahia;
+      mapa.append(data);
 
-      var g = m.select('#Municipios');
+      var municipios = mapa.select('#Municipios');
 
-      $.each(g.selectAll("path").items, function() {
+      $('.mun').bind('contextmenu', function () {
+            let slicedId = this.id.slice(4, 11);
+            let jsondata = fetch(url + slicedId)
+                  .then(res => res.json())
+                  .then(data => jsonResponse = data)
+                  .then(() => location.href = 'index.html')
+            return false;     // Faz o menu não aparecer com o clique do botão direito
+      });
+
+      $.each(municipios.selectAll("path").items, function () {
+
             this.hover(
-                  function () {
-                        let slicedId = this.attr('id').slice(4,11);
-                        let url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" + slicedId;
-                        let jsonResponse;
-
-                        let jsondata = fetch(url)
-                        .then(res => res.json())
-                        .then(data => jsonResponse = data)
-                        .then(() => this.append(Snap.parse('<title><p> Cidade: ' + jsonResponse.nome
+                  () => {
+                        let slicedId = this.attr('id').slice(4, 11);
+                        let jsondata = fetch(url + slicedId)
+                              .then(res => res.json())
+                              .then(data => jsonResponse = data)
+                              .then(() => this.append(Snap.parse('<title>Cidade: ' + jsonResponse.nome
                                     + '&#013 Microrregião: ' + jsonResponse.microrregiao.nome
                                     + '&#013 Mesorregião: ' + jsonResponse.microrregiao.mesorregiao.nome
-                                    + '</title>')))
+                                    + '</title>')));
 
                         this.attr({ 'fill': 'red' });
                   },
-                  function () {
-                        this.attr({ 'fill': '#EEDDB3'});
+                  () => {
+                        this.attr({ 'fill': '#EEDDB3' });
                   }
             )
 
-            this.click(function() {
-                  let slicedId = this.attr('id').slice(4,11);
-                  let url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/" + slicedId;
-                  let jsonResponse;
-
-                  let jsondata = fetch(url)
-                  .then(res => res.json())
-                  .then(data => jsonResponse = data)
-                  .then(() => this.append(alert('Id: ' + jsonResponse.id + '\nCidade: ' + jsonResponse.nome )))
+            this.click(() => {
+                  let slicedId = this.attr('id').slice(4, 11);
+                  let jsondata = fetch(url + slicedId)
+                        .then(res => res.json())
+                        .then(data => jsonResponse = data)
+                        .then(() => this.append(alert('Ir para mesorregião ' + jsonResponse.microrregiao.mesorregiao.nome
+                              + ' de ID ' + jsonResponse.microrregiao.mesorregiao.id)));
             });
       });
 }
