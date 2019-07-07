b1cb17238d2255da2ba9cf99fa7f8b72e4d8910a
Paulo Ricardo <paulo.ricardo@ifba.edu.br>
2019-07-07 17:31:35 -0300

    Div svgimage usando Script JS
    Correção Script JS

--- 2019.1/BibliotecasJS-SVG/SVG.js/main/js/brasil_map.js
+++ 2019.1/BibliotecasJS-SVG/SVG.js/main/js/brasil_map.js
@@ -2,13 +2,14 @@
 
 $(document).ready(function() {
 
-    var image = SVG('svgimage');
+    var image = SVG('drawing');
     $.get('../svg/Brasil.svg', function(contents) {
       var $tmp = $('svg', contents);
       image.svg($tmp.html());
     }, 'xml');
   
     /* Definir funções de tooltip
+    var image = SVG('svgimage');
     $('#svgimage').hover(
       function() {
         image.select('rect').fill('blue');
