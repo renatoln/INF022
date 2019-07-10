3aeb0f268b66681b058c5c7cf6198d99b2a2b406
Mariana Barreiros <maridepp1@hotmail.com>
2019-07-10 18:30:09 -0300

    Adicionada interacao com as micro e mesorregioes

--- 2019.1/BibliotecasJS-SVG/Vanilla/map_svg/scripts/script.js
+++ 2019.1/BibliotecasJS-SVG/Vanilla/map_svg/scripts/script.js
@@ -10,19 +10,21 @@ function toggleHeader(){
     if  (window.pageYOffset > 60 && $header.classList.contains("max-header")){
         $header.classList.remove("max-header");
         $header.classList.add("min-header");
-        $logo.firstElementChild.setAttribute("src", "imgs/omnitech-logo-2.png");
+        //$logo.firstElementChild.setAttribute("src", "imgs/omnitech-logo-2.png");
         $navBar.classList.remove('max-nav');
         $navBar.classList.add('min-nav');
     }
     else if (window.pageYOffset <= 60 && $header.classList.contains("min-header")){
         $header.classList.add("max-header");
         $header.classList.remove("min-header");
-        $logo.firstElementChild.setAttribute("src", "imgs/omnitech-logo-1.png");
+        //$logo.firstElementChild.setAttribute("src", "imgs/omnitech-logo-1.png");
         $navBar.classList.add('max-nav');
         $navBar.classList.remove('min-nav');        
     }
 
 }
+
+//preenche a cidade com o hover e diz o nome
 let vetor = document.getElementsByClassName("mun str2");
 
 for(let i = 0;i < vetor.length;i++){
@@ -38,4 +40,47 @@ for(let i = 0;i < vetor.length;i++){
         vetor[i].style.fill = normalColor;
     });
 }
+
+
+
+//preenche a mesorregião com o hover e diz o nome
+let meso = document.getElementsByClassName("mesoreg");
+
+for(let i = 0; i < meso.length;i++){
+    meso[i].style.fill = normalColor;
+
+    meso[i].addEventListener('click',function(){
+        alert(meso[i].getAttribute("name"));
+    })
+
+    meso[i].addEventListener('mouseover',function(){
+        meso[i].style.fill = hoverColor;
+    })
+
+    meso[i].addEventListener('mouseout',function(){
+        meso[i].style.fill = normalColor;
+    })
+}
+
+//preenche a microrregião com o hover e diz o nome
+let micro = document.getElementsByClassName("micreg");
+//console.log(micro);
+
+for(let i = 0; i < micro.length;i++){
+    micro[i].style.fill = normalColor;
+
+    micro[i].addEventListener('click',function(){
+        alert(micro[i].getAttribute("name"));
+    })
+
+    micro[i].addEventListener('mouseover',function(){
+        micro[i].style.fill = hoverColor;
+    })
+
+    micro[i].addEventListener('mouseout',function(){
+        micro[i].style.fill = normalColor;
+    })
+}
+
+
 
--- 2019.1/BibliotecasJS-SVG/Vanilla/map_svg/scripts/sketch.js
+++ 2019.1/BibliotecasJS-SVG/Vanilla/map_svg/scripts/sketch.js
@@ -1,5 +1,7 @@
 function setup(){
 	loadJSON("http://servicodados.ibge.gov.br/api/v1/localidades/estados/29/municipios",gotData);
+	loadJSON("http://servicodados.ibge.gov.br/api/v1/localidades/estados/29/microrregioes",gotMicro);
+	loadJSON("http://servicodados.ibge.gov.br/api/v1/localidades/estados/29/mesorregioes",gotMeso);
 }
 
 //Essa função pega o nome das cidades no arquivo JSON e passa para o html
@@ -14,5 +16,32 @@ function gotData(data){
 			}
 		}
 	}
+}
 
+function gotMicro(data){
+	let id;
+	for(let i = 0; i < micro.length;i++){
+		id = micro[i].getAttribute("id");
+		let second = id.replace("mic_","");
+		second = second.replace(second[3],"");
+		micro[i].setAttribute("id",second);
+		for(let j = 0; j < data.length; j++){
+			if(data[j].id == second){
+				micro[i].setAttribute("name",data[j].nome);
+			}
+		}
+	}
+}
+
+function gotMeso(data){
+	let id;
+	for(let icount = 0; icount < meso.length;icount++){
+		id = meso[icount].getAttribute("id");
+		id = id.replace("mes_","");
+		for(let jcount = 0; jcount < data.length; jcount++){
+			if(data[jcount].id == id){
+				meso[icount].setAttribute("name",data[jcount].nome);
+			}
+		}
+	}
 }
