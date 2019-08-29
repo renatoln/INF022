db689bb9586e829b944f4d824cebf4562c58c40a
Renato Novais <renatonovais@gmail.com>
2019-08-29 09:32:23 -0300

    adicionados atibutos para o dropdown

--- 2019.1/ProjetoFinal/js/dropdownSelectField.js
+++ 2019.1/ProjetoFinal/js/dropdownSelectField.js
@@ -1,31 +1,38 @@
+//###### Preencher Mesorregiões ######//
 let dropMeso = $('#dropdownMeso');
 
 dropMeso.empty();
 dropMeso.append('<option selected="true">Mesorregião</option>');
 dropMeso.prop('selectedIndex', 0);
 
-const jsonData = getUrlJsonEstadoGeral();
 
-$.getJSON(jsonData, function (data) {
-  $.each(data.MESORREGIOES, function (key, value) {
-    dropMeso.append($('<option></option>').attr('value', value.ID).text(value.NOME_MESORREGIAO));
-  })
-});
+$.each(jsonEstadoGeral.MESORREGIOES, function (key, value) {
+	dropMeso.append($('<option></option>').attr('value', value.ID).text(value.NOME_MESORREGIAO));
+})
 
+//###### Preencher Microrregiões ######//
 let dropMicro = $('#dropdownMicro');
 
 dropMicro.empty();
 dropMicro.append('<option selected="true">Microrregião</option>');
 dropMicro.prop('selectedIndex', 0);
 
-$.getJSON(jsonData, function (data) {
-  $.each(data.MICRORREGIOES, function (key, value) {
-    dropMicro.append($('<option></option>').attr('value', value.ID).text(value.NOME_MICRORREGIAO));
-  })
-});
 
+$.each(jsonEstadoGeral.MICRORREGIOES, function (key, value) {
+	dropMicro.append($('<option></option>').attr('value', value.ID).text(value.NOME_MICRORREGIAO));
+})
+
+
+//###### Preencher Atributos ######//
 let dropAtributos = $('#dropdownAtributos');
 
 dropAtributos.empty();
-dropAtributos.append('<option selected="true">Atributos</option>');
 dropAtributos.prop('selectedIndex', 0);
+
+let atributos = jsonEstadoGeral.MESORREGIOES[0].ATRIBUTOS;
+
+atributos.forEach(myFunction);
+
+function myFunction(item, index) {
+  dropAtributos.append($('<option></option>').attr('value', index).text(item));
+}
